e29f4d543625548149a53db40d48c99875e9df3d
Lakindu Nethsara <lakindunethsara378@gmail.com>
2026-09-17 21:00:33 +0530

    Initial commit

---
+++ src/com/pcl/pos/controller/WelcomeFormController.java
@@ -0,0 +1,16 @@
+package com.pcl.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class WelcomeFormController {
+    public AnchorPane context;
+
+    public void loginFormOnAction(ActionEvent actionEvent) {
+        System.out.println("Clicked Login");
+    }
+
+    public void signupFormOnAction(ActionEvent actionEvent) {
+        System.out.println("Clicked Signup");
+    }
+}
